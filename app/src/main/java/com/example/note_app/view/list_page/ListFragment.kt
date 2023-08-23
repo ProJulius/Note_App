33d73d57e6952d7aba3bf745439ac1d386973a43
ProJulius <projulius141@gmail.com>
2023-08-24 02:17:08 +0700

    Updated 3rd: 24/08/2023

--- app/src/main/java/com/example/note_app/view/list_page/ListFragment.kt
+++ app/src/main/java/com/example/note_app/view/list_page/ListFragment.kt
@@ -2,17 +2,16 @@ package com.example.note_app.view.list_page
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
-import com.example.note_app.interface_callback.AddTaskDialogFragmnetCallback
-import com.example.note_app.constants.CONSTANTS
 import com.example.note_app.R
+import com.example.note_app.constants.CONSTANTS
 import com.example.note_app.databinding.FragmentListBinding
+import com.example.note_app.interface_callback.AddTaskDialogFragmnetCallback
 import com.example.note_app.view.bottom_sheet_dialog.AddTaskDialogFragment
 import kotlin.math.abs
 
@@ -21,10 +20,9 @@ import kotlin.math.abs
 class ListFragment : Fragment() {
     private var _binding: FragmentListBinding? = null
     private val binding get() = _binding!!
-    private var offsetX = 0f
-    private var offsetY = 0f
+    private var startX = 0f
+    private var startY = 0f
     private var isMoving = false
-    private var isAppearing = false
     private var lastClickTime = 0L
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -60,47 +58,45 @@ class ListFragment : Fragment() {
 
     @SuppressLint("ClickableViewAccessibility", "CutPasteId", "ResourceType")
     private fun moveButton() {
-        binding.buttonAdd.setOnTouchListener { view, event ->
+        binding.buttonAdd.setOnTouchListener { _, event ->
             when (event.action) {
                 MotionEvent.ACTION_DOWN -> {
-                    offsetX = event.rawX - view.x
-                    offsetY = event.rawY - view.y
+                    startX = event.x
+                    startY = event.y
                     // Tắt ViewPager2 khi di chuyển nút
                     activity?.findViewById<ViewPager2>(R.id.view_pager_2)?.isUserInputEnabled = false
                     isMoving = false
                 }
 
                 MotionEvent.ACTION_MOVE -> {
-                    val newX = event.rawX - offsetX
-                    val newY = event.rawY - offsetY
-
-                    if (abs(newX) > CONSTANTS.LIMIT_BUTTON_MOVE || abs(newY) > CONSTANTS.LIMIT_BUTTON_MOVE) {
-                        isMoving = true;
+                    val endX = event.x
+                    val endY = event.y
+                    if (abs(endX) > CONSTANTS.LIMIT_BUTTON_MOVE || abs(endY) > CONSTANTS.LIMIT_BUTTON_MOVE) {
+                        isMoving = true
                     }
+
+                    // Lấy kích thước view pager
+                    val fragmentContainer = activity?.findViewById<ViewPager2>(R.id.view_pager_2)
+                    val fragmentWidth = fragmentContainer!!.width
+                    val fragmentHeight = fragmentContainer.height
+
                     if (isMoving) {
-                        // Lấy kích thước view pager
-                        val fragmentContainer =
-                            activity?.findViewById<ViewPager2>(R.id.view_pager_2)
-                        val fragmentWidth = fragmentContainer!!.width
-                        val fragmentHeight = fragmentContainer!!.height
-
-                        // Giới hạn vị trí của nút trong giới hạn màn hình
-                        val maxX = fragmentWidth - view.width
-                        val maxY = fragmentHeight - view.height
-
-                        view.x = newX.coerceIn(0f, maxX.toFloat())
-                        view.y = newY.coerceIn(0f, maxY.toFloat())
+                        var newX: Float = binding.buttonAdd.x + (endX - startX)
+                        var newY: Float = binding.buttonAdd.y + (endY - startX)
+                        newX = newX.coerceIn(0f, fragmentWidth - binding.buttonAdd.width.toFloat())
+                        newY = newY.coerceIn(0f, fragmentHeight - binding.buttonAdd.height.toFloat())
+                        binding.buttonAdd.x = newX
+                        binding.buttonAdd.y = newY
                     }
+
                 }
 
-                MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                MotionEvent.ACTION_UP -> {
                     // Bật lại ViewPager2 khi không di chuyển nút
                     activity?.findViewById<ViewPager2>(R.id.view_pager_2)?.isUserInputEnabled = true
                     if (!isMoving) {
                         addNote()
-                        Log.d("Trung","Fix được đi")
                     }
-                    isAppearing = false
                 }
             }
             true
@@ -110,5 +106,6 @@ class ListFragment : Fragment() {
     fun hideDimmer() {
         binding.dimmer.visibility = View.GONE
     }
+
 
 }
